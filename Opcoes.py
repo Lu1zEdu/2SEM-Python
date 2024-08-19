244f28b1d1cecc71f26f445b2a37c56c8a669179
Lu1zEdu <le6269199@gmail.com>
2024-08-19 08:38:35 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- Opcoes.py
+++ Opcoes.py
@@ -6,10 +6,18 @@ def opcoes():
     print('Sair = = 5')
     escolher_Opcaoo()
 
+
+
 def escolher_Opcaoo():
     escolha= int(input('Escolha uma opções anteriores :  \n'))
     match escolha:
         case 1:
-            print()
+            nome = input("Qual o seu nome :");
+            data = int(input(f"{nome} , quando que vc faz aniversario : \n"));
+            email = input(f"{nome} , qual o seu email :\n");
+
         case 2:
             print()
+
+if __name__ == "__main__":
+    opcoes()
