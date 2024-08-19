435c90602792168771f1d4e26cee723c59ce02e3
Lu1zEdu <le6269199@gmail.com>
2024-08-19 08:27:38 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ Opcoes.py
@@ -0,0 +1,15 @@
+def opcoes():
+    print('Cadastrar = = 1')
+    print('Mostrar = = 2')
+    print('Remover = = 3')
+    print('Atualizar = = 4')
+    print('Sair = = 5')
+    escolher_Opcaoo()
+
+def escolher_Opcaoo():
+    escolha= int(input('Escolha uma opções anteriores :  \n'))
+    match escolha:
+        case 1:
+            print()
+        case 2:
+            print()
