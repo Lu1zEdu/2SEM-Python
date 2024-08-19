d68b64fa5dd64a0d7b9d102a47895fccc77c3b41
Lu1zEdu <le6269199@gmail.com>
2024-08-19 09:30:26 -0300

    Flaertlm

--- Explodir2.py
+++ Explodir2.py
@@ -1,3 +1,4 @@
+contador = 0
 class No :
     lista = [x for x in range(10000)]
     proximo = None
@@ -6,6 +7,8 @@ if __name__  == "__main__" :
     raiz = No()
     temp = raiz
     while True:
+        contador = contador + 1
+        print("Executando consumir : ", contador)
         outro = No()
         temp.proximo = outro
         temp = outro
